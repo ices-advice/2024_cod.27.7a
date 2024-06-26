6ef73316e77c64764db343babb632d348df2ff48
Colin Millar <colinpmillar@gmail.com>
2024-06-26 17:55:35 +0200

    advice 2024

---
+++ 2024_cod.27.7a_assessment/model.R
@@ -0,0 +1,46 @@
+## Run analysis, write model results
+
+## Run analysis, write model results
+
+## Before:
+
+## After: Having run the model
+
+library(icesTAF)
+mkdir("model")
+
+library(r4ss)
+library(ss3om)
+
+
+#------------------------------------------------------------------------------------
+# Create the directory structure where the different model runs will be executed
+#------------------------------------------------------------------------------------
+
+mkdir("model/run")
+
+# copy model executable
+cp(taf.boot.path("software", "ss_win.exe"), "model/run")
+# copy data files
+cp(taf.boot.path("data", "starter.ss"), "model/run")
+cp(taf.boot.path("data", "forecast.ss"), "model/run")
+cp(taf.boot.path("data", "control.ss"), "model/run")
+cp(taf.boot.path("data", "wtatage.ss"), "model/run")
+cp(taf.boot.path("data", "Irishcod_dat.txt"), "model/run")
+
+# this is the folder I have to run ASAP from, after I copied the .exe files there.
+rootwd <- setwd("model/run")
+# run the SS3 model in the model/run folder
+system("ss_win.exe")# runs stock synthesis in this folder.
+# move back to root folder
+setwd(rootwd)
+
+# do the retrospective
+retro(masterdir = file.path(rootwd, "model"), oldsubdir = "run", exe = "ss_win", verbose = TRUE)
+
+# read output into FLR and save
+stk <- readFLSss3("model/run")
+range(stk)["minfbar"] <- 2 #set range to ages 2-4
+range(stk)["maxfbar"] <- 4
+
+save(stk, file = 'model/cod7a_sOSS3.Rdata')
